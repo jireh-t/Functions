64bf8ab33adf14bf93a389d447863f4e01656abd
jireh-t <tsengj@middleton.school.nz>
2022-03-07 11:49:36 +1300

    Update2

--- Movie Tickets.py
+++ Movie Tickets.py
@@ -8,8 +8,30 @@ def main_routine():
     total = 0
     sold = 0
 
-    ticket_wanted = input("Would you like to buy tickets? (Y/N) ")
+    ticket_wanted = input("Would you like to buy tickets? (Y/N) ").upper()
     while ticket_wanted != "N":
-        input("What ticket do you want? \n"
-              "A for adult, S for student, C for child, G for gift voucher.")
+        ticket = input("What ticket do you want? \n"
+              "A for adult, S for student, C for child, G for gift voucher.").upper()
+        amount = int(input("How many would you like? "))
+
+        confirm = input("Would you like to confirm? (Y/N) ").upper()
+        if confirm == "Y":
+            if ticket == "A":
+                adult += amount
+            elif ticket == "S":
+                student += amount
+            elif ticket == "C":
+                adult += amount
+            else:
+                gift += amount
+
+        return("Here is your total so far:\n"
+               f"Adult = {adult}, Student = {student}, Child = {child}, Gift Voucher = {gift}")
+
+
+
+# Main Routine
+print(main_routine())
+
+
 
