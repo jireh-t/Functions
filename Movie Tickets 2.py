ce95b43a3d1bb7239010d18bcc25613b0049da52
jireh-t <tsengj@middleton.school.nz>
2022-03-09 11:55:32 +1300

    new

---
+++ Movie Tickets 2.py
@@ -0,0 +1,65 @@
+#movie tickets program
+
+def main_routine():
+    adult = 0
+    student = 0
+    child = 0
+    gift = 0
+    total = 0
+    sold = 0
+
+    ticket_wanted = input("do you want tickets? ").upper()
+    while ticket_wanted != "N":
+        ticket = sell_ticket()
+
+
+        numT = int(input("How many? "))
+        confirm = input("confirm?").upper()
+
+        if confirm == "Y":
+            price = numT * float(get_price(ticket))
+            total += price
+            sold += numT
+            if ticket == "A":
+                adult += numT
+            elif ticket == "C":
+                child += numT
+            elif ticket == "S":
+                student += numT
+            else:
+                gift += numT
+
+            ticket_wanted = input("\nDo you want "
+                                  "another ticket").upper()
+
+            print("-------------------------------------------------")
+            print(f"the total is {sold}\n"
+                  f"this was made of: \n"
+                  f"\t{adult} for adults\n"
+                  f"\t{student} for students\n"
+                  f"\t{child} for children\n"
+                  f"\t{gift} gift vouchers \n")
+            print(f"sales came to a ${total:.2f}")
+            print("-------------------------------------------------")
+
+def sell_ticket():
+    ticket_type = input("what kind of ticket do you want: \n"
+                        "\tA for adults, or\n"
+                        "\tS for students, or\n"
+                        "\tC for child, or\n"
+                        "\tG for Gift Voucher\n").upper()
+    return ticket_type
+
+
+def get_price(type_):
+    prices = [["A", 12.5]["S", 9]["C", 7]["G",0]]
+    for price in prices:
+        if price[0] == type_:
+            return price[1]
+
+
+
+
+#main routine
+print("********* Fan Fair Movies Ticket Sales *********\n")
+main_routine()
--- Movie Tickets.py
+++ Movie Tickets.py
@@ -13,7 +13,7 @@ def main_routine():
         ticket = input("What ticket do you want? \n"
               "A for adult, S for student, C for child, G for gift voucher.").upper()
         amount = int(input("How many would you like? "))
-
+        amount = globals(amount)
         confirm = input("Would you like to confirm? (Y/N) ").upper()
         if confirm == "Y":
             if ticket == "A":
@@ -25,10 +25,21 @@ def main_routine():
             else:
                 gift += amount
 
-        return("Here is your total so far:\n"
+        return("Here is your total number of tickets so far:\n"
                f"Adult = {adult}, Student = {student}, Child = {child}, Gift Voucher = {gift}")
+    while ticket_wanted != "Y":
+        c_price = 7 * amount
+        s_price = 9 * amount
+        a_price = 12.5 * amount
+        g_price = 0
+        total = c_price + s_price + a_price
 
+        if gift == 0:
+            return(f"Your total price is for\n"
+           f"Adult tickets are {a_price}, child tickets are {c_price}, student tickets are {s_price}, and your total is")
 
+        else:
+            return("Because of gift")
 
 # Main Routine
 print(main_routine())
